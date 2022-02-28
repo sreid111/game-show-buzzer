ae0672432a0cec7ab93e5633c28a1dc6064903ba
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-02-28 10:03:20 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onPinPressed(TouchPin.P0, function () {
 	
 })
-// When a buzzer is the fastest it will receive its serial number back to confirm. Then plays feedback to user. 
+// When a buzzer is the fastest it will receive its serial number back to confirm. Then plays feedback to user.
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == control.deviceSerialNumber()) {
         basic.showIcon(IconNames.Triangle)
@@ -11,22 +11,37 @@ radio.onReceivedNumber(function (receivedNumber) {
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(control.deviceSerialNumber())
     basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(500)
-    basic.showIcon(IconNames.Triangle)
+    basic.pause(200)
 })
-radio.setGroup(11)
 let state = 0
+radio.setGroup(11)
 radio.setTransmitSerialNumber(true)
 radio.sendNumber(control.deviceSerialNumber())
 // state 1 = paired and ready
 // state 2 = you won
-// 
 basic.forever(function () {
     if (state == 1) {
         basic.showIcon(IconNames.Triangle)
     } else if (state == 2) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `,200)
+basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `,50)
+music.playTone(392, music.beat(BeatFraction.Sixteenth))
+            music.playTone(523, music.beat(BeatFraction.Sixteenth))
+        }
         basic.showIcon(IconNames.Happy)
-        music.playTone(262, music.beat(BeatFraction.Whole))
         basic.pause(2000)
         state = 1
     }
